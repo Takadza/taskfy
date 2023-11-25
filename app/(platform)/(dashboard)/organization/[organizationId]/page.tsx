09fa2fae2720da670b32ae5ea8a1b5e3369d6681
Nigel Takadza <56252268+Takadza@users.noreply.github.com>
2023-11-25 23:11:26 +0200

    creation or orginazation

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const OrganizationIDpage = () => {
+    return ( 
+        <div>
+          
+        </div> 
+     );
+}
+ 
+export default OrganizationIDpage; 
